2da869215cfe75a01b4fcc1308d6ba5db8c5751a
ryddiz <71821650+ryddiz@users.noreply.github.com>
2021-03-08 11:35:17 +0100

    breath animation

--- web/accueil.php
+++ web/accueil.php
@@ -1,10 +1,22 @@
 <div class="container-fluid">
   <div class="row" style="padding-top:20em">
-    <div class="circle"></div>
-    <div class="tipDrop">
-      <p>
-        Glissez ou cliquez pour déposer une image de champignon.
-      </p>
-    </div>
+    <div class="circle back"></div>
+    <div class="circle white"></div>
+    <script>
+    var circleBreath = anime({
+      targets: '.back',
+      scale:0.90,
+      loop: true,
+      duration:2000,
+      direction: 'alternate',
+      easing: 'easeInOutSine'
+    });
+    document.querySelector('.back').onclick = circleBreath.play;
+  </script>
+  <div class="tipDrop">
+    <p>
+      Glissez ou cliquez pour déposer une image de champignon.
+    </p>
   </div>
+</div>
 </div>
--- web/index.php
+++ web/index.php
@@ -13,7 +13,8 @@
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
 
-  <script src="index.js"></script>
+  <script src="js/index.js"></script>
+  <script src="js/anime.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
@@ -23,59 +24,70 @@
 <body>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-12">
-        <a class="title" href="./?accueil=on"><h1 style="font-size:60pt;display:inline;">ChampiScan</h1></a>
+      <div class="col-8">
+        <a class="title" href="./?accueil=on"><h1 class="display-1">ChampiScan</h1></a>
         <!--- Dropdown menu --->
-        <i class="fas fa-bars" style="font-size:30pt; float: right; display:inline; margin-top:40px; margin-right:15px; color: #15008f;" data-bs-toggle="dropdown" aria-expanded="false"></i>
+      </div>
+      <div class="col-3"></div>
+      <div class="col-1">
+        <a href="#"  data-bs-toggle="dropdown" aria-expanded="false" style="float: right; height:75px;display:absolute;  text-decoration:none">
+          <i class="fas fa-bars" style="font-size:30pt; float: right; display:inline; margin-top:40px; margin-right:15px; color: #15008f;">
+          </i>
+        </a>
 
 
         <ul class="dropdown-menu">
           <?php
-                if ($_SERVER['REQUEST_URI'] == "/?accueil=on")
-                {
-                  echo "
-                  <li class='dropdown-item'>
-                  <a class='modalLink' href='./?galerie=on'>Galerie</a></li>
-                  <li class='dropdown-item'>
-                  <a class='modalLink' href='./?apropos=on'>A Propos</a></li></ul>";
-                }
-                elseif ($_SERVER['REQUEST_URI'] == "/?apropos=on")
-                {
-                  echo "
-                  <li class='dropdown-item'>
-                  <a class='modalLink' href='./?accueil=on'>Accueil</a></li>
-                  <li class='dropdown-item'>
-                  <a class='modalLink' href='./?galerie=on'>Galerie</a></li></ul>";
-                }
-                elseif ($_SERVER['REQUEST_URI'] == "/?galerie=on")
-                {
-                  echo "
-                  <li class='dropdown-item'>
-                  <a class='modalLink' href='./?accueil=on'>Accueil</a></li>
-                  <li class='dropdown-item'>
-                  <a class='modalLink' href='./?apropos=on'>A Propos</a></li>";
-                }
-                else echo "<li class='dropdown-item'>
-                <a class='modalLink' href='./?galerie=on'>Galerie</a></li>
-                <li class='dropdown-item'>
-                <a class='modalLink' href='./?apropos=on'>A Propos</a></li></ul>";
-                ?>
+          if ($_SERVER['REQUEST_URI'] == "/?accueil=on")
+          {
+            echo "
+            <li class='dropdown-item'>
+            <a class='modalLink' href='./?galerie=on'>Galerie</a></li>
+            <li class='dropdown-item'>
+            <a class='modalLink' href='./?apropos=on'>A Propos</a></li></ul>";
+          }
+          elseif ($_SERVER['REQUEST_URI'] == "/?apropos=on")
+          {
+            echo "
+            <li class='dropdown-item'>
+            <a class='modalLink' href='./?accueil=on'>Accueil</a></li>
+            <li class='dropdown-item'>
+            <a class='modalLink' href='./?galerie=on'>Galerie</a></li></ul>";
+          }
+          elseif ($_SERVER['REQUEST_URI'] == "/?galerie=on")
+          {
+            echo "
+            <li class='dropdown-item'>
+            <a class='modalLink' href='./?accueil=on'>Accueil</a></li>
+            <li class='dropdown-item'>
+            <a class='modalLink' href='./?apropos=on'>A Propos</a></li>";
+          }
+          else echo "<li class='dropdown-item'>
+          <a class='modalLink' href='./?galerie=on'>Galerie</a></li>
+          <li class='dropdown-item'>
+          <a class='modalLink' href='./?apropos=on'>A Propos</a></li></ul>";
+          ?>
 
-              </div>
-            </div>
-          </div>
         </div>
-        <!---Fin modal -->
       </div>
     </div>
   </div>
+  <!---Fin modal -->
+</div>
+</div>
+</div>
 <?php
-  if (get("galerie")) include("galerie.php");
-  elseif (get("apropos")) include("apropos.php");
-  elseif (get("infoChampi")) include("infoChampi.php");
-  else include("accueil.php");
+if (get("galerie")) include("galerie.php");
+elseif (get("apropos")) include("apropos.php");
+elseif (get("infoChampi")) include("infoChampi.php");
+else include("accueil.php");
 ?>
 
 
+<footer class="footer">
+  <div class="container-fluid">
+    <h5>Projet sur le Machine Learning. Site web créé par Logan Moriniere.</h5>
+  </div>
+</footer>
 </body>
 </html>

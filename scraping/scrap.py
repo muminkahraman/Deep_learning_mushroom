4026f48cb6ba8683b594c6f73d4470037fbc4397
muminkahraman <mumin@DESKTOP-7SMCVG1.localdomain>
2021-03-28 22:46:46 +0200

    avancement dans la préparation du dataset. apparement l'arborécence qur j'avais préparé n'était pas bonne.j'ai donc modifié ça avec un script python. maintenant les photos sont rangé dans deux repertoires train et test. les dossiers train est et test contiennent chacun un dossier par champignon. le partage a été fait avec 70% 30%. j'ai aussi mis de coté les champignins qui ne contenaient pas plus de 100 photos. J'ai suivi et avancé sur le tuto transfer learning de tensorflow. Si tout marche bien, demain premiers essais d'entrainements.

--- scraping/scrap.py
+++ scraping/scrap.py
@@ -2,6 +2,10 @@ import csv
 import urllib.request
 import os
 from urllib.error import HTTPError
+import shutil
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
 
 def telecharger():
     t = 254921
@@ -29,11 +33,65 @@ def telecharger():
         print("error number = " + str(e))
 
 def cut_two():
-    #print(os.listdir("dataset/brut/"))
+    dele()
     for doss in os.listdir("dataset/brut/"):
+        liste = os.listdir("dataset/brut/" + doss + "/")
+        le = int((len(liste)))
+        t = int((len(liste))*0.7)
+        s = (len(liste))- t
+        print("dataset/brut/" + doss + "/" )
         if not os.path.exists("dataset/brut/" + doss + "/" + "train"):
             os.makedirs("dataset/brut/" + doss + "/" + "train")
         if not os.path.exists("dataset/brut/" + doss + "/" + "test"):
             os.makedirs("dataset/brut/" + doss + "/" + "test")
-        t = len("dataset/brut/" + doss))//0.7
+        for i in range(t):
+            print("t = " + str(t) + "    s = " +str(s) +"    i = " + str(i)+ "    len = " + str(le))
+            shutil.move(("dataset/brut/" + doss + "/" + liste[i]), ("dataset/brut/" + doss + "/" + "train" + "/" + liste[i]))
+            print("train" + "/" + liste[i] + "  ------  done")
+        for i in range(t, le):
+            print("t = " + str(t) + "    s = " +str(s) +"    i = " + str(i)+ "    len = " + str(le))
+            shutil.move(("dataset/brut/" + doss + "/" + liste[i]), ("dataset/brut/" + doss + "/" + "test" + "/" + liste[i]))
+            print("test" + "/" + liste[i] + "  ------  done")
 
+def dele():
+    for doss in os.listdir("dataset/brut/"):
+        if os.path.exists("dataset/brut/" + doss + "/" + "train"):
+            os.rmdir("dataset/brut/" + doss + "/" + "train")
+        if os.path.exists("dataset/brut/" + doss + "/" + "test"):
+            os.rmdir("dataset/brut/" + doss + "/" + "test")
+
+def number_filter():
+    if not os.path.exists("dataset_filtered1"):
+        os.makedirs("dataset_filtered1");
+    s = 0
+    n = 0
+    for doss in os.listdir("dataset/brut/"):
+        liste_tr = os.listdir('dataset/brut/' + doss + '/train/')
+        liste_te = os.listdir('dataset/brut/' + doss + '/test/')
+        tr = len(liste_tr)
+        te = len(liste_te)       
+        c = te + tr
+        #c = len([name for name in os.listdir('dataset/brut/' + doss + '/train') if os.path.isfile(name) ])
+        #c = c + len([name for name in os.listdir('dataset/brut/' + doss + '/test') if os.path.isfile(name)])
+        #print(c)
+        if c >= 100:
+            if not os.path.exists("dataset_filtered1/train/" + doss + "/"):
+                os.makedirs("dataset_filtered1/train/" + doss + "/");
+            if not os.path.exists("dataset_filtered1/test/" + doss + "/"):
+                os.makedirs("dataset_filtered1/test/" + doss + "/");
+            for im in liste_tr:
+                shutil.move(("dataset/brut/" + doss + "/train/" + im), ("dataset_filtered1/train/" + doss + "/" + im))
+                print("moving   dataset/brut/" + doss + "/train/" + im + "   =>   dataset_filtered1/train/" + doss + "/" + im)
+            for im in liste_te:
+                shutil.move(("dataset/brut/" + doss + "/test/" + im), ("dataset_filtered1/test/" + doss + "/" + im))
+                print("moving   dataset/brut/" + doss + "/test/" + im + "   =>   dataset_filtered1/test/" + doss + "/" + im)
+            s +=1
+            n = n + c
+    print (str(s) + ' ------------ ' + str(n))
+
+def test():
+    l=[]
+    for i in range(0,20):
+        l.append(i)
+    print(l)
+    print(len(l))

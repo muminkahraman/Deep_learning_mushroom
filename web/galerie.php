683215fb28259a7be1f90eccce8194594d04a18e
ryddiz <71821650+ryddiz@users.noreply.github.com>
2021-03-31 11:58:20 +0200

    galerie changes

--- web/galerie.php
+++ web/galerie.php
@@ -1,38 +1,59 @@
-<div class="container-fluid grid px-4">
-  <div class="row gy-5">
+<div class="container-fluid">
+  <div class="row mx-auto mt-3" style="width:40%;">
+    <input class="bar" id="search" onkeyup="mushSearch()" type="text" placeholder=" Search" />
+  </div>
+</div>
 
-    <?php
 
-    if(isset($_GET['page']) && !empty($_GET['page']))
-    {
-      $currentPage = (int) strip_tags($_GET['page']);
-    }
-    else
-    {
-      $currentPage = 1;
-    }
+<?php
 
-    echo (int)strip_tags($_GET['page']);
+if(isset($_GET['page']) && !empty($_GET['page']))
+{
+  $currentPage = (int) strip_tags($_GET['page']);
+}
+else
+{
+  $currentPage = 1;
+}
 
-    $string = file_get_contents("ressources/names/champName.json");
-    $json_a = json_decode($string, true);
 
-    $size = count($json_a[mush]);
+$string = file_get_contents("ressources/names/champName.json");
+$json_a = json_decode($string, true);
 
-    $perPage = 100;
+$arr = [];
 
-    $pages = ceil($size/ $perPage);
+foreach ($json_a[mush] as $key => $value) {
+  array_push($arr, $value[Name]);
+}
 
-    $premier = ($currentPage* $perPage) - $perPage;
-    /*
-    for ($i=$premier; $i < $perPage; $i++) {
-    echo '
-    <div class="col-3">
-    <div class="p-3 border bg-light">';
-    echo $json_a[mush][$i][Name];
-    echo '</div>
+
+sort($arr);
+
+$size = count($json_a[mush]);
+
+$perPage = 4000;
+
+$pages = ceil($size/ $perPage);
+
+$premier = ($currentPage* $perPage) - $perPage;
+
+$limit=3;
+
+
+
+echo '
+<div class="container-fluid grid px-4 mt-5" >
+  <div class="row gy-5" id="listMush"> ';
+
+for ($i=$premier; $i < $size; $i++) {
+  echo '
+    <div class="col-2" >
+      <a href="#" class="border">IMG</a>
+        <div><h7>';  echo $arr[$i];
+        echo '</h7>
+        </div>
     </div>';
-  };*/
-  ?>
+};
+?>
 </div>
 </div>
--- web/js/index.js
+++ web/js/index.js
@@ -1,3 +1,20 @@
-function myFunction(x) {
-  x.classList.toggle("change");
-}
+function mushSearch() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('search');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("listMush");
+  li = ul.getElementsByTagName('div');
+  console.log(li);
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("h7")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+  }

174286171c2597aa41c59a5a886e775b44f9524f
mumin kahraman <mumin.kahraman@etu.univ-lyon1.fr>
2021-04-21 22:46:36 +0200

    avancée nettoyage des données.

--- Tuto thibaut tensorflow/remove useless folders.py
+++ Tuto thibaut tensorflow/remove useless folders.py
@@ -10,11 +10,8 @@ if not os.path.exists("../dataset/unusable/"):
 for mush in os.listdir(base_dir):
     taille = len(os.listdir("../dataset/brut/" + mush + "/"))
     if taille < limite:
-        """
-        if not os.path.exists("../dataset/unusable/" + mush):
-            os.makedirs("../dataset/unusable/" + mush)
-        shutil.rmtree(os.path.join(base_dir, mush))
-        """
+
+        shutil.move((r"../dataset/brut/" + mush + "/"), (r"../dataset/unusable/" + mush + "/"))
     print(mush + ' done')
 
 

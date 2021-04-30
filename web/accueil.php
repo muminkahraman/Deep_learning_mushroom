8d5989b6e489e63a3ce75aa05d877d50acf57d48
Logan <logan.moriniere@hotmail.fr>
2021-04-30 15:49:42 +0200

    amelioration predict

--- web/accueil.php
+++ web/accueil.php
@@ -1,4 +1,3 @@
-
 <div class="container-fluid">
   <div class="row" style="padding-top:20em">
     <div class="circle back"></div>
@@ -14,163 +13,183 @@
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body" id="body">
-            <img id="imgOutput" height="200" width="200" src="">
-            <table class="table table striped">
-              <thead>
-                <tr><th>Nom du champignon</th><th>Pourcentage de fiabilité</th>
-                </thead>
-                <tbody id="tableBody">
-                </tbody>
-              </table>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
-            </div>
+            <img id="imgOutput" height="200" src="">
+            <button id="predictBtn" class="btn btn-primary float-right">
+              Predict
+            </button>
+            <table class="table table-striped" id="table">
+              <tbody id="tableBody">
+              </tbody>
+            </table>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
           </div>
         </div>
       </div>
-
-      <form action="#">
-        <div id="drop_zone" class="circle white" ondrop="dropHandler(event);" ondragover="dragOverHandler(event);">
-          <img draggable="false" class="shroom" src="ressources/image/shroomCrop.png"/>
-          <input id="inputIMG" type="file" name="file" size="1" onchange="onFileSelected(event); "/>
-        </div>
-      </form>
-
-      <script>
-
-      function dragOverHandler(ev) {
-        console.log('File(s) in drop zone');
-
-        // Prevent default behavior (Prevent file from being opened)
-        ev.preventDefault();
-
-      }
-
-      function dropHandler(ev)
-      {
-        console.log('File(s) dropped');
-
-        // Prevent default behavior (Prevent file from being opened)
-        ev.preventDefault();
-
-        if (ev.dataTransfer.items)
-        {
-          // Use DataTransferItemList interface to access the file(s)
-          for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-            // If dropped items aren't files, reject them
-            if (ev.dataTransfer.items[i].kind === 'file') {
-              var file = ev.dataTransfer.items[i].getAsFile();
-              //console.log('... file[' + i + '].name = ' + file.name);
-            }
-          }
-        }
-        else
-        {
-          // Use DataTransfer interface to access the file(s)
-          for (var i = 0; i < ev.dataTransfer.files.length; i++)
-          {
-            //console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
-          }
-        }
-
-
-      }
-
-      function onFileSelected(event) {
-        var file= event;
-        var input = file.target;
-        var reader = new FileReader();
-        reader.onload = function(){
-          var dataURL = reader.result;
-          var output = document.getElementById('imgOutput');
-          output.setAttribute("src", dataURL);
-        };
-        reader.readAsDataURL(input.files[0]);
-
-        const output = document.getElementById('imgOutput');
-        const tfImg = tf.browser.fromPixels(output);
-        const smallImg = tf.image.resizeBilinear(tfImg, [224,224]);
-        const resized = tf.cast(smallImg, 'float32');
-        const t4d = tf.tensor4d(Array.from(resized.dataSync()),[1,224,224,3])
-
-        async function load() {
-          const model = await tf.loadGraphModel('ressources/model/model.json');
-          return model;
-        };
-
-        function predict(model)
-        {
-          let top3 = [];
-          model.then(model=>
-            {
-              let result = model.predict(t4d).data();
-              result.then(function(res)
-              {
-                res.forEach(function(item,i)
-                {
-                  top3.push( { name: classM[i], prob : item });
-
-                });
-
-                top3.sort(function(a,b)
-                {
-                  return b.prob - a.prob ;
-                })
-
-                top3 = top3.slice(0,3);
-                let tbody = document.getElementById("tableBody");
-                top3.forEach(function(item)
-                {
-                  let p = Math.round(item.prob*10000)/100;
-                  var newRow = tbody.insertRow();
-                  var nameCell = newRow.insertCell();
-                  nameCell.appendChild(document.createTextNode(item.name));
-                  var probCell = newRow.insertCell();
-                  probCell.appendChild(document.createTextNode(p));
-                });
-
-
-              });
-
-
-            });
-
-
-          $('#mushModal').modal('show');
-
-          };
-
-          const model = load();
-          predict(model);
-
-    };
-
-
-
-        var myModalEl = document.getElementById('mushModal')
-        myModalEl.addEventListener('hidden.bs.modal', function (event) {
-          let tbody = document.getElementById("tableBody");
-        })
-
-        </script>
-
-        <script>
-        var circleBreath = anime({
-          targets: '.back',
-          scale:0.90,
-          loop: true,
-          duration:2000,
-          direction: 'alternate',
-          easing: 'easeInOutSine'
-        });
-        document.querySelector('.back').onclick = circleBreath.play;
-        </script>
-
-        <div class="tipDrop">
-          <p>
-            Glissez ou cliquez pour déposer une image de champignon.
-          </p>
-        </div>
-      </div>
     </div>
+
+    <form action="#">
+      <div id="drop_zone" class="circle white" ondrop="dropHandler(event);" ondragover="dragOverHandler(event);">
+        <img draggable="false" class="shroom" src="ressources/image/shroomCrop.png"/>
+        <input id="inputIMG" type="file" name="file" size="1"  accept="image/png"/>
+      </div>
+    </form>
+
+    <script>
+
+    function dragOverHandler(ev) {
+      console.log('File(s) in drop zone');
+      ev.preventDefault();
+
+    }
+
+    function dropHandler(ev)
+    {
+      console.log('File(s) dropped');
+
+      ev.preventDefault();
+
+      if (ev.dataTransfer.items)
+      {
+        // Use DataTransferItemList interface to access the file(s)
+        for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+          // If dropped items aren't files, reject them
+          if (ev.dataTransfer.items[i].kind === 'file') {
+            var file = ev.dataTransfer.items[i].getAsFile();
+            console.log('... file[' + i + '].name = ' + file.name);
+          }
+        }
+      }
+      else
+      {
+        // Use DataTransfer interface to access the file(s)
+        for (var i = 0; i < ev.dataTransfer.files.length; i++)
+        {
+          console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+        }
+      }
+
+
+      let reader = new FileReader();
+      reader.onload = function () {
+        let dataURL = reader.result;
+        $("#imgOutput").attr("src", dataURL);
+        $("#prediction-list").empty();
+      }
+      let file2 = $("#inputIMG").prop("files")[0];
+      reader.readAsDataURL(file2);
+
+      $('#mushModal').modal('show');
+
+    }
+
+
+    $("#inputIMG").change(function () {
+      let reader = new FileReader();
+      reader.onload = function () {
+        let dataURL = reader.result;
+        $("#imgOutput").attr("src", dataURL);
+        $("#prediction-list").empty();
+      }
+      let file = $("#inputIMG").prop("files")[0];
+      reader.readAsDataURL(file);
+
+      $('#mushModal').modal('show');
+
+    });
+
+    let model;
+    (async function () {
+      model = await tf.loadGraphModel('ressources/model/model.json');
+      $(".progress-bar").hide();
+    })();
+
+
+    $("#predictBtn").click(async function () {
+      let btn = document.getElementById("predictBtn");
+      btn.classList.add("disabled");
+      let image = $('#imgOutput').get(0);
+
+      let pre_image = tf.browser.fromPixels(image, 3)
+      .resizeNearestNeighbor([224, 224])
+      .expandDims()
+      .toFloat()
+      let predict_result = await model.predict(pre_image).data();
+
+      let order = Array.from(predict_result)
+      .map(function (p, i) {
+        return {
+          prob: p,
+          name: classM[i]
+        };
+      }).sort(function (a, b) {
+        return b.prob - a.prob;
+      }).slice(0, 3);
+
+      var table = document.getElementById("table");
+      let tbody = document.getElementById("tableBody");
+      var thead= document.createElement('thead');
+      thead.innerHTML="<tr><th>Nom</th><th>Similarité</th></tr>";
+      thead.setAttribute("id", "thead");
+      table.appendChild(thead);
+      order.forEach(function(item)
+      {
+        let p = Math.round(item.prob*10000)/100;
+        if ( p > 5)
+        {
+          var newRow = tbody.insertRow();
+          let name = item.name;
+          var nameCell = newRow.insertCell();
+
+          var a = document.createElement('a');
+          var txt = document.createTextNode(name);
+          a.setAttribute("href", "/?infoMush=on&name="+item.name);
+          a.classList.add('text-black');
+          a.setAttribute("style", "text-decoration: none");
+          a.appendChild(txt);
+          nameCell.appendChild(a);
+          var probCell = newRow.insertCell();
+          probCell.appendChild(document.createTextNode(p));
+        }
+
+      });
+
+    });
+
+
+
+    var myModalEl = document.getElementById('mushModal')
+    myModalEl.addEventListener('hidden.bs.modal', function (event) {
+      var thead = document.getElementById("thead");
+      if (thead != null)
+      {
+        thead.remove();
+      }
+      let tbody = document.getElementById("tableBody");
+      tbody.innerHTML="";
+      let btn = document.getElementById("predictBtn");
+      btn.classList.remove("disabled");
+    })
+
+  </script>
+
+  <script>
+  var circleBreath = anime({
+    targets: '.back',
+    scale:0.90,
+    loop: true,
+    duration:2000,
+    direction: 'alternate',
+    easing: 'easeInOutSine'
+  });
+  document.querySelector('.back').onclick = circleBreath.play;
+</script>
+
+<div class="tipDrop">
+  <p>
+    Glissez ou cliquez pour déposer une image de champignon.
+  </p>
+</div>
+</div>
+</div>
--- web/index.php
+++ web/index.php
@@ -20,6 +20,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
    <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js"></script>
+   
 </head>
 <body>
   <div class="container-fluid">
